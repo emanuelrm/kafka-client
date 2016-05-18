6bd6f0dfae54b8fc224939571c9dc1a51590997d
Emanuel Rocha <emanuel.melechco@redetendencia.com.br>
2016-05-18 18:50:46 -0400

    comit

---
+++ src/main/java/br/com/benchmark/kafka/integracaokafka/Produtor.java
@@ -0,0 +1,36 @@
+package br.com.benchmark.kafka.integracaokafka;
+
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+
+public class Produtor {
+	
+	public static void main(String[] args) {
+		
+		 Properties props = new Properties();
+		 props.put("bootstrap.servers", "localhost:9092");
+		 props.put("acks", "all");
+		 props.put("retries", 0);
+		 props.put("batch.size", 16384);
+		 props.put("auto.commit.interval.ms", 1000);
+		 props.put("linger.ms", 1);
+		 props.put("buffer.memory", 33554432);
+		 props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		 props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		 props.put("block.on.buffer.full", "true");
+		 
+		 Producer<String, String> producer = new KafkaProducer<>(props);
+		 for(int i = 1; i < 1000000; i++) {
+		     producer.send(new ProducerRecord<String, String>("test", "newmsg - " + Integer.toString(i)));
+		 }
+
+//		 producer.send(new ProducerRecord<String, String>("test", "newmsg2"));
+		 System.out.println("Fim");
+		 producer.close();
+	}
+
+}
